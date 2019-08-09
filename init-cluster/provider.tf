e8f0033c188b5065e2e3d94fb13dc804d206e898
Antoine Meausoone <antoine.meausoone@gmail.com>
2019-08-09 09:39:42 +0200

    feat(terraform): setup in simple lightweight cluster

---
+++ init-cluster/provider.tf
@@ -0,0 +1,15 @@
+
+variable "sfeirschool_k8s_gcp_projectid" {
+  type = "string"
+}
+
+variable "gcp_zone"{
+  default="europe-west3-a"
+}
+
+provider "google" {
+  project = "${var.sfeirschool_k8s_gcp_projectid}"
+  // Frankfurt
+  region = "${var.gcp_zone}"
+  credentials = "${file("~/.gcp/terraform-sfeirschool-k8s.json")}"
+}
